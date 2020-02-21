f4d85f3f6a8e171930ec6298626827b4acce3c92
hsq2019 <55921438+hsq2019@users.noreply.github.com>
2020-02-21 18:15:25 +0800

    反转一个单链表
    输入: 1->2->3->4->5->NULL
    输出: 5->4->3->2->1->NULL

---
+++ ReverseList.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null){
+            return null;
+        }
+        if(head.next==null){
+            return head;
+        }
+        ListNode prev=null;
+        ListNode cur=head;
+        while(cur!=null){
+            ListNode next=cur.next;
+            cur.next=prev;
+            prev=cur;
+            cur=next;
+        }
+        head=prev;
+        return head;
+    }
+}
