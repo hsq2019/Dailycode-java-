387d35b7a13694905d277b5572be08fb1fd9f2b2
hsq2019 <55921438+hsq2019@users.noreply.github.com>
2020-03-03 15:48:56 +0800

    简单的窗口(练习)
    java实现一个简单的窗口

---
+++ Test2.java
@@ -0,0 +1,15 @@
+package java_learn;
+import javax.swing.JFrame;
+
+public class Test2 {
+    public static void main(String[] args) {
+        JFrame frame1=new JFrame();
+        frame1.setTitle("Window 1");
+        frame1.setSize(400,300);
+        frame1.setLocation(200,100);
+        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame1.setVisible(true);
+
+
+    }
+}
