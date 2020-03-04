260f163697fdcc3ec8653b92a3499bacdc0a02e5
hsq2019 <55921438+hsq2019@users.noreply.github.com>
2020-03-04 18:08:20 +0800

    冒泡排序
    冒泡排序有关的代码。时间复杂度O(n^2)

---
+++ BubbleSort.java
@@ -0,0 +1,23 @@
+package datastructure.sort;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr={3,9,-1,10,-2};
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=0;
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+            System.out.println("第"+(i+1)+"次排序");
+            for(int x:arr){
+                System.out.print(x+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
