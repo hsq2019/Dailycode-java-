5aa0de2cccf18faf129ad93cba3d8ee512928ebb
hsq2019 <55921438+hsq2019@users.noreply.github.com>
2020-03-05 19:38:19 +0800

    选择排序
    选择排序，时间复杂度是n^2

---
+++ SelectSort.java
@@ -0,0 +1,28 @@
+package datastructure.sort;
+
+import java.util.Arrays;
+
+public class SelectSort {
+    public static void main(String[] args) {
+        int[] arr={109,11,5,111};
+        selectSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    //选择排序
+    public static void selectSort(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            int minIndex=i;
+            int min=arr[i];
+            for(int j=i+1;j<arr.length-1;j++){
+                if(min>arr[j]) {
+                    min = arr[j];
+                    minIndex = j;
+                }
+            }
+            if(arr[minIndex]!=arr[i]){
+                arr[minIndex]=arr[i];
+                arr[i]=min;
+            }
+        }
+    }
+}
