0f57f6e480a6faaceae1c556ea815d2f01cf7a7a
hsq2019 <55921438+hsq2019@users.noreply.github.com>
2020-03-15 19:19:05 +0800

    基数排序
    基数排序是桶排序的扩展。将所有比较比较数值统一为同样的长度，从最低位开始，依次进行一次排序，这样从最低位一直到最高为排序完成后，数列会变成一个有序序列

---
+++ RadixSort.java
@@ -0,0 +1,46 @@
+package datastructure.sort;
+
+import java.util.Arrays;
+
+//基数排序（桶排序的扩展）
+public class RadixSort {
+    public static void main(String[] args) {
+        int[] arr = {53, 3, 452, 748, 14, 214};
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        int maxLength = (max + "").length();
+        radixSort(arr, maxLength);
+    }
+
+    public static void radixSort(int[] arr, int maxLength) {
+        //桶
+        int[][] bucket = new int[10][arr.length];
+        //记录每个同的有效数字个数
+        int[] bucketElementCount = new int[10];
+        //给桶里放数据
+        for (int i = 0; i < maxLength; i++) {
+
+            for (int y = 0; y < arr.length; y++) {
+                int digit = (arr[y] / (int) Math.pow(10, i)) % 10;
+                bucket[digit][bucketElementCount[digit]] = arr[y];
+                bucketElementCount[digit]++;
+            }
+            //把数据重新放回arr数组中
+            int num = 0;
+            for (int x = 0; x < bucketElementCount.length; x++) {
+                if (bucketElementCount[x] != 0) {
+                    for (int l = 0; l < bucketElementCount[x]; l++) {
+                        arr[num] = bucket[x][l];
+                        num++;
+                    }
+                }
+                bucketElementCount[x] = 0;
+            }
+            System.out.println("第" + (i + 1) + "次处理,arr是" + Arrays.toString(arr));
+        }
+    }
+}
