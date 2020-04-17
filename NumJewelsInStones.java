cfad455d6cbe82b7e0e627c253ec207033dc32be
hsq2019 <1484607030@qq.com>
2020-04-17 17:43:51 +0800

    前K个高频单词

--- NumJewelsInStones.java
+++ NumJewelsInStones.java
@@ -1,6 +1,7 @@
 class Solution {
     //输入: J = "aA", S = "aAAbbbb"
     //输出: 3
+    //set的好处是可以快速判断元素是否在set中存在
     public int numJewelsInStones(String J, String S) {
         Set<Character> set=new HashSet<>();
         int res=0;
