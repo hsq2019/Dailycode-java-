e832ac2698701a22d3e5d1af24319ed253751e63
hsq2019 <1484607030@qq.com>
2020-05-17 20:41:00 +0800

    线程测试

---
+++ ThreadTest.java
@@ -0,0 +1,16 @@
+public class ThreadTest {
+    //�̲߳���
+    public static void main(String[] args) throws InterruptedException {
+        String name = null;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(999999999999L);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        },name="good").start();
+    }
+}
