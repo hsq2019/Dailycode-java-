6f98064cc50d437d1c59133063a96e22983c3ac7
hsq2019 <1484607030@qq.com>
2020-06-10 17:28:53 +0800

    牛客练习求最小公倍数

--- ideaCode/src/niuke/MinimumCommonMultiple.java
+++ ideaCode/src/niuke/MinimumCommonMultiple.java
@@ -1,4 +1,28 @@
 package niuke;
 
+import java.util.Scanner;
+
+//最小公倍数
 public class MinimumCommonMultiple {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        if(x<y){
+            int temp=x;
+            x=y;
+            y=temp;
+        }
+        int num=help(x,y);
+        int res=(x/num)*y;
+        System.out.println(res);
+    }
+    //辗转相除法
+    public static int help(int big,int small){
+        if(small==0){
+            return big;
+        }
+        int r=big%small;
+        return help(small,r);
+    }
 }
