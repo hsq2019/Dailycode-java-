ba63e407f75eaefdda69b4111e789e0e299d69f4
hsq2019 <1484607030@qq.com>
2020-06-11 17:16:17 +0800

    牛客练习 一串数字中最小的k个数

--- ideaCode/src/niuke/MinimumNumberOfK.java
+++ ideaCode/src/niuke/MinimumNumberOfK.java
@@ -1,4 +1,26 @@
 package niuke;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+//每个测试输入包含空格分隔的n+1个整数，最后一个整数为k值,n不超过100
+
 public class MinimumNumberOfK {
+    //一串数字中最小的k个数
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while (sc.hasNextLine()){
+            String s=sc.nextLine();
+            String[] arr=s.split(" ");
+            int n=Integer.valueOf(arr[arr.length-1]);
+            int[] nums=new int[arr.length-1];
+            for(int i=0;i< nums.length;i++){
+                nums[i]=Integer.valueOf(arr[i]);
+            }
+            Arrays.sort(nums);
+            for(int i=0;i<n;i++){
+                System.out.print(nums[i]+" ");
+            }
+        }
+    }
 }
