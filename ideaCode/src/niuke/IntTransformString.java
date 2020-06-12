93e2dcb073183719cbe23b85b6be33426fedb44d
hsq2019 <1484607030@qq.com>
2020-06-12 16:13:07 +0800

    整数转化成逆置字符串

--- ideaCode/src/niuke/IntTransformString.java
+++ ideaCode/src/niuke/IntTransformString.java
@@ -1,4 +1,16 @@
 package niuke;
 
-public class InttransformString {
+import java.util.Scanner;
+
+//int转化成逆置字符串
+//例子 100-----001
+public class IntTransformString {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String s=String.valueOf(n);
+        StringBuilder sb=new StringBuilder(s);
+        s=sb.reverse().toString();
+        System.out.println(s);
+    }
 }
