f3d69ef8f73529c15f4757cb8468c2e615f3d32a
hsq2019 <1484607030@qq.com>
2020-06-18 15:29:39 +0800

    牛客练习格雷编码

--- ideaCode/src/niuke/GrayCode.java
+++ ideaCode/src/niuke/GrayCode.java
@@ -1,4 +1,24 @@
 package niuke;
 
 public class GrayCode {
+        public static String[] getGray(int n) {
+            int i=(int)Math.pow(2,n);
+            String[] arr=new String[i];
+            for(int x=0;x<arr.length;x++){
+                StringBuffer sb=new StringBuffer();
+                for(int j=0;j<n;j++){
+                    sb.append((x>>j)%2);
+                    sb.reverse();
+                }
+                arr[x]=sb.toString();
+            }
+            return arr;
+        }
+
+    public static void main(String[] args) {
+        String[] arr=getGray(3);
+       for(String s:arr){
+           System.out.println(s);
+       }
+    }
 }
