5d8b0baa68911cec6bf29af9b0242dde18b11680
hsq2019 <1484607030@qq.com>
2020-06-18 15:39:48 +0800

    牛客练习长整数的加法

--- ideaCode/src/niuke/Add.java
+++ ideaCode/src/niuke/Add.java
@@ -1,4 +1,21 @@
 package niuke;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Add {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String b=sc.nextLine();
+        System.out.println(AddLongInteger(a,b));
+    }
+    public static String AddLongInteger(String a,String b){
+//        char[] arr1=a.toCharArray();
+//        char[] arr2=b.toCharArray();
+        BigInteger c=new BigInteger(a);
+        BigInteger d=new BigInteger(b);
+        BigInteger sum=c.add(d);
+        return sum.toString();
+    }
 }
