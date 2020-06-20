ce6db902a0e34f91cf8c2f010be0df1aae6f3151
hsq2019 <1484607030@qq.com>
2020-06-20 18:08:10 +0800

    牛客练习正方形

--- ideaCode/src/niuke/Square.java
+++ ideaCode/src/niuke/Square.java
@@ -1,4 +1,26 @@
 package niuke;
 
+import java.util.Scanner;
+
 public class Square {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        char c=sc.next().charAt(0);
+        for(int i=1;i<=n;i++){
+            System.out.print(c);
+        }
+        System.out.println();
+        for(int i=1;i<=Math.ceil((double)n/2)-2;i++){
+            System.out.print(c);
+            for(int j=2;j<=n-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println(c);
+        }
+        for(int i=1;i<=n;i++){
+            System.out.print(c);
+        }
+    }
 }
