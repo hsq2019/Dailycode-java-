e27afd5b4c891d8276fdd1b9dd2096c38eb61991
hsq2019 <1484607030@qq.com>
2020-09-12 18:52:43 +0800

    牛客练习，二叉树的最大深度

--- ideaCode/src/niuke/MaxDepth.java
+++ ideaCode/src/niuke/MaxDepth.java
@@ -1,6 +1,8 @@
 package niuke;
 
 
+import java.util.LinkedList;
+import java.util.Queue;
 
 class TreeNode {
    int val = 0;
@@ -15,5 +17,24 @@ public class MaxDepth {
             return 0;
         }
         return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
+    public int maxDepth1(TreeNode root){
+        int res=0;
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            int size=queue.size();
+            for(int i=0;i<size;i++){
+                TreeNode node=queue.poll();
+                if(node.left!=null){
+                    queue.add(node.left);
+                }
+                if(node.right!=null){
+                    queue.add(node.right);
+                }
+            }
+            res++;
+        }
+        return res;
     }
 }
